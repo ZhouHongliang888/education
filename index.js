2a0263650b4412cfacccdf2b87187604f42252a1
ZhouHongliang888 <103399890+ZhouHongliang888@users.noreply.github.com>
2022-04-26 21:01:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+<html>
+<body>
+
+<h2>Click here to display the date and time</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+click here
+</button>
+
+<p id="demo"></p>
+
+</body>
+</html>
